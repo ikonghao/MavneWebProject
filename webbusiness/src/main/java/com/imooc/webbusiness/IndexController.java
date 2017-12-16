0c246c46e640f70c7bf92e55f2d896f3cf18fbe9
konghao <konghao8013@163.com>
2017-12-16 16:49:28 +0800

    spring MVC 参数三种接受方式

--- webbusiness/src/main/java/com/imooc/webbusiness/IndexController.java
+++ webbusiness/src/main/java/com/imooc/webbusiness/IndexController.java
@@ -1,10 +1,15 @@
 package com.imooc.webbusiness;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,16 +28,28 @@ public class IndexController {
 
 	private static Logger log = LoggerFactory.getLogger(IndexController.class);
 
-/*	@RequestMapping("/index")
-	public String index() {
-		return "index";
-	}*/
-
+	/*
+	 * @RequestMapping("/index") public String index() { return "index"; }
+	 */
 	@RequestMapping(value = "index", method = RequestMethod.GET)
 	public String index(@RequestParam("id") int id, Model model) {
 		log.debug("in viewCourse,id={}", id);
 		IndexEntity entity = indexServer.getIndexEntityByID(id);
-		model.addAttribute("entity",entity);
+		model.addAttribute("entity", entity);
+		return "index";
+	}
+
+	@RequestMapping(value = "index2/{id}", method = RequestMethod.GET)
+	public String index2(@PathVariable("id") int id, Map<String, Object> model) {
+		IndexEntity entity = indexServer.getIndexEntityByID(id);
+		model.put("entity", entity);
+		return "index";
+	}
+@RequestMapping("index3")
+	public String index3(HttpServletRequest request) {
+		int id = Integer.valueOf(request.getParameter("id"));
+		IndexEntity entity = indexServer.getIndexEntityByID(id);
+		request.setAttribute("entity", entity);
 		return "index";
 	}
 }
