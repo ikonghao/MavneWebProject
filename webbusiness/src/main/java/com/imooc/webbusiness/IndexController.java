cc45e567f929d553ddf381aca7caab0be89b3da5
konghao <konghao8013@163.com>
2017-12-17 12:24:02 +0800

    增加上传文件功能

--- webbusiness/src/main/java/com/imooc/webbusiness/IndexController.java
+++ webbusiness/src/main/java/com/imooc/webbusiness/IndexController.java
@@ -1,9 +1,13 @@
 package com.imooc.webbusiness;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.imooc.webbusiness.entity.IndexEntity;
 import com.imooc.webbusiness.servlet.IndexService;
@@ -63,5 +70,25 @@ public class IndexController {
 	@RequestMapping("save")
 	public String save(IndexEntity entity) {
 		return "redirect:index3?id=180";
+	}
+
+	@RequestMapping("file")
+	public String file() {
+		return "file";
+
+	}
+
+	/* 上传文件 */
+	@RequestMapping(value = "doUpload", method = RequestMethod.POST)
+	public String doUpload(@RequestParam("file") MultipartFile file) throws IOException {
+		
+		if (!file.isEmpty()) {
+			log.debug("正在处理文件,{}" + file.getOriginalFilename());
+			String fileName="F:\\Code\\git\\MavneWebProject\\testFile\\"+System.currentTimeMillis()+file.getOriginalFilename();
+			FileUtils.copyInputStreamToFile(file.getInputStream(),new File(fileName));
+			
+		}
+		
+		return "success";
 	}
 }
